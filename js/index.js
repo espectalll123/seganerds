e2ebf377867606d1a01752ff5ba8765b401e1388
Francisco Gómez <espectalll123@kydara.com>
2015-09-17 14:00:46 +0200

    BIG rewrital of the app - get hyped!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /*
 SEGA Nerds app for Chrome-compatible systems
-2014 espectalll123 <espectalll123@outlook.com>
+2014-2015 espectalll123 <espectalll123@kydara.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -17,9 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function getIndex() {
-	var content = document.createElement("div");
-    	content.id = "content";
-	document.body.appendChild(content);
+	$(".fa-circle-o-notch").css("opacity", 1); // Have to rewrite with Angular.js
 	$.ajax({
 		url: "http://www.seganerds.com/?json=1",
 	})
@@ -27,12 +25,12 @@ function getIndex() {
 		for (i = 0; i < data.posts.length; i++) {
 			$("#content").append('<h1>' + data.posts[i].title + '</h1>');
 		}
+		$(".fa-circle-o-notch").css("opacity", 0);
     	})
 	.fail(function(data) {
-		var content = document.createElement("div");
-    		content.id = "content";
-		document.body.appendChild(content);
-		$("#content").append('<img src="img/ohno.png" /><br />');
-		$("#content").append('<p class="error">Something went wrong!</p>');	
+		$("#content").append('<div class="alert alert-danger">');
+		$('.alert-danger').append('<strong>Whoopsie!</strong>');
+		$('.alert-danger').append(' It seems that all your base are belong to us');
+		$(".fa-circle-o-notch").css("opacity", 0);
 	});
 };
