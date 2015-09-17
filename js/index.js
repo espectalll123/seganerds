7414def503a0fb2e8348f4faa0b99cb0a9224d85
Francisco Gómez <espectalll123@kydara.com>
2015-09-17 12:00:20 +0200

    It gets better! (like _really_ better)

--- js/index.js
+++ js/index.js
@@ -17,70 +17,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function getIndex() {
+	var content = document.createElement("div");
+    	content.id = "content";
+	document.body.appendChild(content);
 	$.ajax({
 		url: "http://www.seganerds.com/?json=1",
-		/* The readyState bug must be fixed in the future.
-		Meanwhile, let's enjoy some awkwardness!!! :D*/
-	}).done(function(data) {
-		console.log('wat');
+	})
+	.done(function(data) {
+		for (i = 0; i < data.posts.length; i++) {
+			$("#content").append('<h1>' + data.posts[i].title + '</h1>');
+		}
+    	})
+	.fail(function(data) {
 		var content = document.createElement("div");
     		content.id = "content";
 		document.body.appendChild(content);
-		$("#content").append(data);
-		console.log(data);
-    	});
-    	/* error: function(data) {
-    		if (data.readyState == 4) {
-    			// displayIndex(data.responseText.split("</script>\n")[1]);
-    		} else {
-    			var content = document.createElement("div");
-    			content.id = "content";
-    			var error = document.createElement("p");
-    			error.className = "error";
-    			var ohno = document.createElement("img");
-    			ohno.src = "img/ohno.png";
-    			var br = document.createElement("br");
-    			var error_text = document.createTextNode(
-    				"Something went wrong!");
-    			error.appendChild(ohno);
-    			error.appendChild(br);
-    			error.appendChild(error_text);
-    			content.appendChild(error);
-    			document.body.appendChild(content);
-    		};
-    	}});*/
+		$("#content").append('<img src="img/ohno.png" /><br />');
+		$("#content").append('<p class="error">Something went wrong!</p>');	
+	});
 };
-function displayIndex(content) {
-	index = jQuery.parseJSON(content);
-	var postList = "";
-	var content = document.createElement("div");
-	content.id = "content";
-	for (var i = 0; i < index.length; i++) {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", index[i].the_post_thumbnail[0], true);
-		xhr.responseType = "blob";
-		xhr.onload = function(e) {
-  			var img = document.createElement("img");
-  			img.className = "post-image";
-  			img.src = window.URL.createObjectURL(this.response);
-  			content.appendChild(img);
-		};
-		xhr.send();
-		var h1 = document.createElement("h1");
-		var h1_text = document.createTextNode(
-			Encoder.htmlDecode(index[i].the_title));
-		h1.appendChild(h1_text);
-		content.appendChild(h1);
-		var p = document.createElement("p");
-		p.className = "description";
-		var p_text = document.createTextNode(
-			Encoder.htmlDecode(
-			index[i].the_excerpt.replace(
-			" [&hellip", "") + "..."));
-		p.appendChild(p_text);
-		content.appendChild(p);
-	};
-	document.body.appendChild(content);
-};
-
-getIndex()
--- js/window.js
+++ js/window.js
@@ -35,49 +35,49 @@ $(document).ready(function() {
 			$("#dragBar").css({"width": winWidth-424});
 		};
 	});
-    $("#minBtn").click(function(){
-        thisWindow.minimize()
-    }); 
-    $("#closeBtn").click(function(){
-        thisWindow.close()
-    });
-    $("#tmBox").mouseenter(function(){
-        $("#titleMenu").css({"display": "block"});
-        $("#titleMenu").animate({opacity: 1}, 100);
-    });
-    $("#searchBtn").click(function(){
-        if (searchIs) {
-        	$("#dragBar").css({"width": $(window).width()-424});
-        	$("#dragBar").css({"left": "296px"});
-        	$("#searchBox").animate({width: 0}, 100, function(){
-        		$(this).css({"display": "none"});
-        	});
-        	searchIs = false;
-        } else {
-        	$("#dragBar").css({"width": $(window).width()-680});
-        	$("#dragBar").css({"left": "552px"});
-        	$("#searchBox").animate({width: "238px"}, 100);
-        	$("#searchBox").css({"display": "block"});
-        	searchIs = true;
-        };
-    });
-    $(".tmClickable").click(function(){
-    	$("#titleMenu").animate({opacity: 0}, 100, function(){
-    		$(this).css({"display": "none"});
-    	});
-    });
-    $("#tmFS").click(function(){
-    	if (!thisWindow.isFullscreen()) {
-        	thisWindow.fullscreen();
-        	$("#minBtn").css({"display": "none"});
-        } else {
-        	thisWindow.restore();
-        	$("#minBtn").css({"display": "block"});
-        };
-    });
-    $("#titleMenu").mouseleave(function(){
-    	$("#titleMenu").animate({opacity: 0}, 100, function(){
-    		$(this).css({"display": "none"});
-    	});
-    });
+	$("#minBtn").click(function(){
+		thisWindow.minimize()
+	}); 
+	$("#closeBtn").click(function(){
+		thisWindow.close()
+	});
+	$("#tmBox").mouseenter(function(){
+		$("#titleMenu").css({"display": "block"});
+		$("#titleMenu").animate({opacity: 1}, 100);
+	});
+	$("#searchBtn").click(function(){
+		if (searchIs) {
+			$("#dragBar").css({"width": $(window).width()-424});
+			$("#dragBar").css({"left": "296px"});
+			$("#searchBox").animate({width: 0}, 100, function(){
+				$(this).css({"display": "none"});
+			});
+			searchIs = false;
+		} else {
+			$("#dragBar").css({"width": $(window).width()-680});
+			$("#dragBar").css({"left": "552px"});
+			$("#searchBox").animate({width: "238px"}, 100);
+			$("#searchBox").css({"display": "block"});
+			searchIs = true;
+		};
+	});
+	$(".tmClickable").click(function(){
+		$("#titleMenu").animate({opacity: 0}, 100, function(){
+			$(this).css({"display": "none"});
+		});
+	});
+	$("#tmFS").click(function(){
+		if (!thisWindow.isFullscreen()) {
+			thisWindow.fullscreen();
+			$("#minBtn").css({"display": "none"});
+		} else {
+			thisWindow.restore();
+			$("#minBtn").css({"display": "block"});
+		};
+	});
+	$("#titleMenu").mouseleave(function(){
+		$("#titleMenu").animate({opacity: 0}, 100, function(){
+			$(this).css({"display": "none"});
+		});
+	});
 });
